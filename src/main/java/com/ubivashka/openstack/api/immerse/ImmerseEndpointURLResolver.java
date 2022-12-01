7241254c0e879f49c658a5cbfe1814a2ce4fa2ca
U61vashka <botyrbojey@gmail.com>
2022-12-02 00:02:59 +0600

    Resolve only BlockStorage endpoint in immerse support

--- src/main/java/com/ubivashka/openstack/api/immerse/ImmerseEndpointURLResolver.java
+++ src/main/java/com/ubivashka/openstack/api/immerse/ImmerseEndpointURLResolver.java
@@ -1,11 +1,15 @@
 package com.ubivashka.openstack.api.immerse;
 
+import org.openstack4j.api.types.ServiceType;
 import org.openstack4j.model.identity.URLResolverParams;
 import org.openstack4j.openstack.identity.internal.DefaultEndpointURLResolver;
 
 public class ImmerseEndpointURLResolver extends DefaultEndpointURLResolver {
     @Override
     public String findURLV3(URLResolverParams p) {
-        return super.findURLV3(p).replace("v2", "v3");
+        String result = super.findURLV3(p);
+        if (p.type == ServiceType.BLOCK_STORAGE)
+            return result.replace("v2", "v3");
+        return result;
     }
 }
